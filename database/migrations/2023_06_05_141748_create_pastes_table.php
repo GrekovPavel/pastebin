b4683ff23310f8e2c40bdad49bcd045d88a9dbcf
Pavel Grekov <p.grekov@5th.ru>
2023-06-07 00:32:57 +0700

    fix: editing field in a table

--- database/migrations/2023_06_05_141748_create_pastes_table.php
+++ database/migrations/2023_06_05_141748_create_pastes_table.php
@@ -17,6 +17,7 @@ class CreatePastesTable extends Migration
             $table->id();
             $table->text('content');
             $table->time('life_time')->default(0);
+            $table->string('link');
             $table->string('visibility');
             $table->timestamps();
         });
