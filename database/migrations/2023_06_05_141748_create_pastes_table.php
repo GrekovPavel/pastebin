0800796f91ba7db7450fbf81edb8e52b4f83a569
Pavel Grekov <p.grekov@5th.ru>
2023-06-06 19:56:57 +0700

    feat: create migration. Pastes table

---
+++ database/migrations/2023_06_05_141748_create_pastes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastes', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->time('life_time');
+            $table->string('visibility');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pastes');
+    }
+}
