0eae3759579953682a324902d0df0d7fa3b31657
Pavel Grekov <p.grekov@5th.ru>
2023-06-06 19:38:04 +0700

    feat: create Paste controller and model

---
+++ app/Http/Controllers/PasteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PasteController extends Controller
+{
+    public function index() {
+        return 'index page';
+    }
+}
